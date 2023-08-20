61dafca433c7fe412f874b9af1acf7c6438965e0
csark0812 <csark0812@yahoo.com>
2023-08-20 12:02:07 -0700

    Added in basic API capabilities

---
+++ frontend/src/components/LoginScreen.js
@@ -0,0 +1,10 @@
+const LoginScreen = () => {
+    return (
+      <div>
+        <h2>Login Screen</h2>
+        {/* Add your login form here */}
+      </div>
+    );
+  };
+  
+  export default LoginScreen;
